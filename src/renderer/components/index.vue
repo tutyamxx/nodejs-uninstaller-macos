f8856e1f800236966f2d5cb1fc0a06a7bcfcd982
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2020-02-21 15:32:49 +0000

    Added notifications and improved prompt description

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -28,39 +28,20 @@ import util from "util";
 
 const remote = require("electron").remote;
 const exec = util.promisify(require("child_process").exec);
+const notifier = require("node-notifier");
+const path = require("path");
 
 const bIsRoot = (process.getuid && process.getuid() === 0);
 
 // --| Until I find a better way to run multiple sudo commands at once without prompting the password for every single command, this should work fine :/
 const MacRemoveNodeFromPaths =
 [
-   "rm -rf ~/.npm \
-   && rm -rf ~/.nvm \
-   && rm -rf ~/.npmrc \
-   && rm -rf ~/.node-gyp \
-   && rm -rf ~/.node_repl_history \
-   && rm -rf /usr/local/bin/node \
-   && rm -rf /usr/local/bin/npm \
-   && rm -rf /usr/local/bin/npx \
-   && rm -rf /usr/local/bin/node-debug \
-   && rm -rf /usr/local/bin/node-gyp \
-   && rm -rf /usr/local/lib/node \
-   && rm -rf /usr/local/lib/node_modules \
-   && rm -rf /usr/local/include/node \
-   && rm -rf /usr/local/include/node_modules \
-   && rm -rf /usr/local/lib/dtrace/node.d \
-   && rm -rf /usr/local/share/doc/node \
-   && rm -rf /usr/local/share/systemtap/tapset/node.stp \
-   && rm -rf /usr/local/lib/node_modules \
-   && rm -rf /opt/local/bin/node \
-   && rm -rf /opt/local/bin/node_modules \
-   && rm -rf /opt/local/include/node \
-   && rm -rf /opt/local/include/node_modules \
-   && rm -rf /opt/local/lib/node_modules \
-   && rm -rf /opt/local/lib/node \
-   && rm -rf /usr/local/share/man/man1/node* \
-   && rm -rf /usr/local/share/man/man1/npm* \
-   && rm -rf /usr/local/share/man/man1/npx*"
+    "rm -rf ~/.npm ~/.nvm ~/.npmrc ~/.node-gyp ~/.node_repl_history \
+    && rm -rf /usr/local/bin/node /usr/local/bin/npm /usr/local/bin/npx /usr/local/bin/node-debug /usr/local/bin/node-gyp \
+    && rm -rf /usr/local/lib/node /usr/local/lib/node_modules /usr/local/include/node /usr/local/include/node_modules /usr/local/lib/dtrace/node.d \
+    && rm -rf /usr/local/share/doc/node /usr/local/share/systemtap/tapset/node.stp /usr/local/lib/node_modules \
+    && rm -rf /opt/local/bin/node /opt/local/bin/node_modules /opt/local/include/node /opt/local/include/node_modules /opt/local/lib/node_modules /opt/local/lib/node \
+    && rm -rf /usr/local/share/man/man1/node* /usr/local/share/man/man1/npm* /usr/local/share/man/man1/npx*"
 ];
 
 export default
@@ -101,29 +82,60 @@ export default
             {
                 this.uninstall_progress_percentage = this.get_random_num(20, 35);
 
-                const ShellCommandPrompt = `/usr/bin/osascript -e 'do shell script "bash -c \\\"${MacRemoveNodeFromPaths}\\\"" with administrator privileges'`;
+                const ShellCommandPrompt = `/usr/bin/osascript -e \
+                    'do shell script "bash -c \\\"${MacRemoveNodeFromPaths}\\\"" \
+                    with prompt "Node.JS Uninstaller needs the administrator privileges in order to completely remove Node.JS from your system" with administrator privileges'`;
 
                 try
                 {
                     this.uninstall_progress_percentage = this.get_random_num(35, 60);
+
+                    await notifier.notify(
+                    {
+                        title: "Node.JS Uninstaller",
+                        message: "♻️ Uninstalling Node.JS ...",
+                        icon: path.join(__dirname, "../assets/images/uninstall.png"),
+                        contentImage: path.join(__dirname, "../assets/images/garbagebin.png"),
+                        timeout: 2
+                    });
+
                     await this.execute_terminal_command(ShellCommandPrompt);
 
                     this.uninstall_progress_percentage = this.get_random_num(61, 90);
                     await this.execute_terminal_command("sleep 1.1");
 
                     this.uninstall_progress_percentage = 100;
-                    await this.execute_terminal_command("sleep 0.6");
                     await this.execute_terminal_command("afplay /System/Library/Sounds/Submarine.aiff -v 10");
+                    await this.execute_terminal_command("sleep 1");
+
+                    await notifier.notify(
+                    {
+                        title: "Node.JS Uninstaller",
+                        message: "♻️ Uninstall complete!",
+                        icon: path.join(__dirname, "../assets/images/uninstall.png"),
+                        contentImage: path.join(__dirname, "../assets/images/garbagebin.png"),
+                        timeout: 2
+                    });             
                 }
 
                 catch(error)
                 {
                     this.uninstall_started = false;
                     this.uninstall_progress_percentage = 0;
+
+                    await notifier.notify(
+                    {
+                        title: "Node.JS Uninstaller",
+                        message: "🚫 Uninstaller stopped!",
+                        icon: path.join(__dirname, "../assets/images/uninstall.png"),
+                        contentImage: path.join(__dirname, "../assets/images/garbagebin.png"),
+                        timeout: 2
+                    });        
                 }
             }
 
             this.uninstall_started = false;
+            this.uninstall_progress_percentage = 0;
         }
     }
 }
